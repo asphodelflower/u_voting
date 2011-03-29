0ea74742845bbe55713e0e142b7406ba20c84040
asphodelflower <asphodelflower@gmail.com>
2011-03-29 04:37:02 +0300

    Improved admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,11 @@ class UsersController < ApplicationController
   def create
     hobo_create do
       if valid?
-        self.current_user = this
-        flash[:notice] = t("hobo.messages.you_are_site_admin", :default=>"You are now the site administrator")
-        redirect_to home_page
+        #self.current_user = this
+        #flash[:notice] = t("hobo.messages.you_are_site_admin", :default=>"You are now the site administrator")
+        #redirect_to home_page
 
-        flash[:notice] = "User created successfully"
+        #flash[:notice] = "User created successfully"
         redirect_to(object_url(this))
       end
     end
